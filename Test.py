ab6a3f64a3d1024439a45e75cc70ff374db4e87c
Downtrod <100236948+Downtrod@users.noreply.github.com>
2022-02-25 11:28:36 +1300

    DTE2022
    The first course / tutorial we have in DTE

---
+++ Test.py
@@ -0,0 +1,83 @@
+import random
+print("e World")
+print("aaaa")
+print(10+2)
+
+
+print(99104*23)
+print(24/4)
+
+
+
+print("My name is Charlie Melville")
+print("My teachers name is Kris White, known as Mrs White")
+print("The school I am in is caleld Kings High School")
+print("I am currently 15 years old.")
+print("I have approximately 6,000 hours on my highest played game, and 200 hours on my favourite game.")
+print(2022 + 25)
+age = 15
+print(age + 100)
+print(658/7)
+year=2022
+print(year-1969)
+print("My favourite game is Phantom Forces.")
+print("My favourite class is English and Math.")
+
+
+favouritegame = input("What is your favourite game?")
+
+'''
+answer1=("Absolutely!")
+answer2=("No way Pedro!")
+answer3=("Go for it tiger.")
+answer4=("Aw hell nah dawg you aint wanna do dat oh hell nahhh dog.")
+answer5=("I'm uncertain, shake me again (yes)")
+
+print("Welcome to the Magic 8 Ball game - use it to answer your questions...\n")
+question = input("Ask me for any advice and I'll try and help you out. Type in your question and then press Enter.\n")
+
+print("shaking.... " * 4)
+choice=random.randint(1,5)
+
+if choice ==1:
+  answer=answer1
+elif choice==2:
+  answer=answer2
+elif choice == 3:
+  answer=answer3
+elif choice == 4:
+  answer=answer4
+else:
+  answer=answer5
+print(answer)
+reroll = int(input("Restart? (press 1 if yes, 2 if no)"))
+while reroll ==1:
+  if reroll ==1:
+    if reroll ==1:
+      answer1=("Absolutely!")
+  answer2=("No way Pedro!\n")
+  answer3=("Go for it tiger.\n")
+  answer4=("Aw hell nah dawg you aint wanna do dat oh hell nahhh dog.\n")
+  answer5=("I'm uncertain, shake me again (yes)\n")
+
+  print(f"Welcome to the Magic 8 Ball game - use it to answer your questions...")
+  print()
+  question = input("Ask me for any advice and I'll try and help you out.\n Type in your question and then press Enter.\n")
+
+  print("shaking.... \n" * 4)
+  choice=random.randint(1,5)
+
+  if choice ==1:
+    answer=answer1
+  elif choice==2:
+    answer=answer2
+  elif choice == 3:
+    answer=answer3
+  elif choice == 4:
+    answer=answer4
+  else:
+    answer=answer5
+  print(answer)
+  reroll = int(input("Restart? (press 1 if yes, 2 if no)"))
+'''
+
