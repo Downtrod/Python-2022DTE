d739dc9c012b0f7c90e6d395594e2697a62dac01
Downtrod <100236948+Downtrod@users.noreply.github.com>
2022-03-08 13:25:25 +1300

    Continued into exercise 8.7
    Unable to complete the last question.

---
+++ 8.7_exercises.py
@@ -0,0 +1,51 @@
+#Question 1
+fibonacci = [0,1,1,2,3,4,5,13,21,34]
+print(fibonacci)
+print("")
+#Question 2
+favouritefoods = ["Tomato", "Beetroot", "Apples", "Banana", "Coconut"]
+print(favouritefoods)
+print("")
+#Question 3
+youtoobers = ["ashanedesilva", "ninja", "dobrebrothers", "fortnite_youtuber69_9", "ricegum"]
+print(youtoobers)
+print("")
+#Question 4
+favouritesongs = []
+favouritesongs.append("Sabaton-NightWitches")
+favouritesongs.append("PoloGBadMan")
+favouritesongs.append("EminemWithoutMe")
+favouritesongs.append("WeDrinkYourBlood")
+favouritesongs.append("RedSunInTheSky")
+print(favouritesongs)
+print("")
+#Question 5
+books = []
+books.append(input("What are your favourite songs ( five of them)\n"))
+books.append(input("\n"))
+books.append(input("\n"))
+books.append(input("\n"))
+books.append(input("\n"))
+print(books)
+#Question 6
+pizza_topping = []
+pizza_topping.append(input("What pizza toppings do you want? (Up to 6?\n"))
+pizza_topping.append(input("\n"))
+pizza_topping.append(input("\n"))
+pizza_topping.append(input("\n"))
+pizza_topping.append(input("\n"))
+pizza_topping.append(input("\n"))
+
+while pizza_topping == "":
+	pizza_topping.remove("")
+print(pizza_topping)
+
+
+
+
+
+
+
+
+
+
