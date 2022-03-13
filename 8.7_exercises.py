ed57da67965454244df01dc975ffc35ec40c2f61
Downtrod <100236948+Downtrod@users.noreply.github.com>
2022-03-14 10:58:25 +1300

    Continuedonfor9.2and9.3
    Progress did not go through on my laptop during the rowing regatta so I was unable to finish 9.2 and 9.3 at this time, will completely finish next period.

--- 8.7_exercises.py
+++ 8.7_exercises.py
@@ -19,6 +19,7 @@ favouritesongs.append("WeDrinkYourBlood")
 favouritesongs.append("RedSunInTheSky")
 print(favouritesongs)
 print("")
+
 #Question 5
 books = []
 books.append(input("What are your favourite songs ( five of them)\n"))
@@ -30,16 +31,46 @@ print(books)
 #Question 6
 pizza_topping = []
 pizza_topping.append(input("What pizza toppings do you want? (Up to 6?\n"))
-pizza_topping.append(input("\n"))
-pizza_topping.append(input("\n"))
-pizza_topping.append(input("\n"))
-pizza_topping.append(input("\n"))
-pizza_topping.append(input("\n"))
+pizza_topping.append(input())
+pizza_topping.append(input())
+pizza_topping.append(input())
+pizza_topping.append(input())
+pizza_topping.append(input())
 
-while pizza_topping == "":
+while "" in pizza_topping:
 	pizza_topping.remove("")
 print(pizza_topping)
+print("")
 
+#Question 7
+fruits = ["tomato", "apple", "coconut", "banana", "beetroot"]
+playerfruit = input("What type of fruit do you want? If We have it we will give it to you.")
+if playerfruit.lower() in fruits:
+	print("We have that!")
+else:
+	print("Unfortunately we do not have that fruit")
+	fruits.append(playerfruit)
+print(fruits)
+print("")
+
+#Question 8
+names = ["John", "Dave", "Matt", "Amy", "Poppy", "Ahmed"]
+names.sort()
+print(names)
+names.reverse()
+print(names)
+print("")
+
+#Question 9
+prime_numbers= [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,]
+print(prime_numbers)
+print(f"The length of the list is")
+print(len(prime_numbers))
+
+#Question 10 
+common_verbs = ["be", "have", "do", "say", "get", "make", "go"]
+print(common_verbs)
+print("")
 
 
 
---
+++ 9.2and9.3_exercises.py
@@ -0,0 +1,44 @@
+#9.2
+#Question 1
+order_complete = "False"
+toppings_available = ["vanilla","strawberry","chocolate","sprinkles","nuts","raisins","flake","m&ms"]
+
+topping_count = 0
+while order_complete == "False":
+	choice = input("What sort of toppings do you want? pick from - \n" + toppings_available.join())
+	if choice.lower() == toppings_available:
+		print(f"We've added {choice}")
+		topping_count=topping_count+1
+
+	if topping_count == 6:
+		order_complete == "True"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
